b1d00ef0e73587cc4b72e21a21cd4417fb2dde26
fatelei <fatelei@gmail.com>
2022-06-04 14:46:05 +0800

    first commit

---
+++ cmd/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"flag"
+	"github.com/fatelei/store-layer-demo/pkg/fs"
+)
+
+func main() {
+	var mountpoint string
+	flag.StringVar(&mountpoint, "mountpoint", "", "mountpoint")
+	flag.Parse()
+	if len(mountpoint) == 0 {
+		return
+	}
+
+	store := &fs.LayerStore{}
+	err := fs.NewLayerStore(mountpoint, store)
+	if err != nil {
+		panic(err)
+	}
+}
